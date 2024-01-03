4117b3332b7f6f63fcf70c96b42d0d957a60e9bd
Tamara <tamaraavgust14@gmail.com>
2024-01-03 16:05:42 +0700

    Использование override

--- Driver.h
+++ Driver.h
@@ -17,7 +17,7 @@ public:
 	Driver(std::string name);
 	Driver(int balance);
 	Driver(std::string name, int balance, int experience, int orderAmount);
-	void input();
+	void input() override;
 	Driver& operator=(Person& person);
 	std::ostream& operator << (std::ostream& os);
 };
