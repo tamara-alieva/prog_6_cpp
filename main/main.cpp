2ce4301f9e18fb71415e6c882b8a6889e80f124e
Tamara <tamaraavgust14@gmail.com>
2024-01-03 17:25:52 +0700

    Тесты виртуальной и невиртуальной функций

--- main/main.cpp
+++ main/main.cpp
@@ -10,19 +10,48 @@ using namespace std;
 
 int main() {
     cout << "TESTS:" << endl << endl;
-    /*Passenger passenger{ "Ivan" };
-    passenger.input();
+    /*
+    
+    cout << "## Calling base (Person) class's overloaded method (with calling Person::input() method)" << endl;
+    Passenger passenger1{ "Ivan" };
+    passenger1.input();
 
+    cout << "## Calling base (Person) class's overloaded method (without calling Person::input() method)" << endl;
     Driver driver{};
     driver.input();
 
-    Passenger pass1{ "Georgiy", 5000, true };
-    pass1.output();*/
+    cout << "## Calling base (Person) class's constructor" << endl;
+    Passenger passenger2{ "Georgiy", 5000, true };
+    passenger2 << cout;
+    
+    */
 
+    cout << "## Overloaded operator= for Derived classes" << endl;
     Person person{ "Kirill", 7000 };
-    Passenger pass2{};
-    pass2 = person;
-    pass2.output();
+    Passenger passenger3{};
+    passenger3 = person;
+    passenger3 << cout;
+    
+    cout << "## Virtual and non-virtual functions" << endl << endl;
+
+    cout << "# Calling virtual function for dynamic object" << endl;
+    Person* pers1{ &person };
+    Driver driver1{};
+    pers1 = &driver1;
+    pers1->input();
+
+    cout << "# Calling non-virtual function for dynamic object" << endl;
+    Passenger pass3{"Nikolay", 7000, true};
+    pers1 = &pass3;
+    pers1->output();
+
+    cout << endl << "# Calling virtual function from non-virtual" << endl;
+    pers1 = &driver1;
+    pers1->test_virtual();
+
+    cout << endl << "# Calling non-virtual function from non-virtual" << endl;
+    pers1 = &pass3;
+    pers1->test_non_virtual();
 
     return 0;
 }
